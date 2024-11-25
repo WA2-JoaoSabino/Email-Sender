db275b86e3497157709e08fc25b1720a358cc0e9
WA2-JoaoSabino <jsabino@wa2.business>
2024-11-25 20:56:59 -0300

    Corrigido função do botão "Enviar Emails" e adicionada mensagem de confirmação.

--- mainwindow.py
+++ mainwindow.py
@@ -17,24 +17,44 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.buttonSendEmail.clicked.connect(self.resetText)
+        self.ui.buttonSendEmail.clicked.connect(self.enviar_emails)
         self.ui.buttonSelectFile.clicked.connect(self.selecionar_planilha)
 
 
-    def resetText(self):
+    def resetar_email(self):
         try:
-            self.ui.inputText.setPlainText("Insira seu texto aqui")
+            resposta = QMessageBox.question(
+                self,
+                "Aviso",
+                "Esta ação vai apagar todo o texto na caixa acima. Tem certeza de que deseja resetar o seu e-mail?",
+                QMessageBox.Yes | QMessageBox.No
+            )
+
+            if resposta == QMessageBox.Yes:
+                self.ui.inputText.setPlainText("Insira seu texto aqui")
+
         except Exception as e:
             print(f"Erro ao resetar o texto: {e}")
 
     def enviar_emails(self):
-        ...
+        try:
+            ...
+        except Exception as e:
+            print(f"Não foi possível enviar os emails: {e}")
 
     def selecionar_planilha(self):
         try:
             caminho_arquivo = QFileDialog.getOpenFileName(
                 self, 'Selecione a planilha bacana com os CNPJs', ''
             )
+
+            if len(caminho_arquivo) == 0:
+                QMessageBox.information(
+                    self,
+                    "Erro!",
+                    "Nenhum arquivo foi selecionado"
+                )
+                return
 
             QMessageBox.information(
                 self,
@@ -54,6 +74,17 @@ class MainWindow(QMainWindow):
         except Exception as e:
             print(f"Erro ao selecionar planilha: {e}")
 
+    def atualizar_dataframe(self):
+        try:
+            print("Ainda não implementado!")
+        except Exception as e:
+            print(f"Erro ao atualizar os dados no dataframe: {e}")
+
+    def atualizar_lista(self):
+        try:
+            print("Ainda não implementado!")
+        except Exception as e:
+            print(f"Erro ao atualizar a lista de empresas: {e}")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
